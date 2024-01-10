c60e374310eb83c5775b70192bd6a10a3927fbc2
Daniel Abril <danbril1015@gmail.com>
2024-01-10 18:12:04 -0500

    add dto user and get controller

---
+++ FoodieFam-Back/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using FoodieFam_Back.DTOs;
+using FoodieFam_Back.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodieFam_Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private FoodieFamContext _context;
+        public UserController(FoodieFamContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserDto>> GetUsers() =>
+            await _context.Users.Select(user => new UserDto
+            {
+                UserId = user.UserId,
+                Name = user.Name,
+                LastName = user.LastName,
+                Email = user.Email,
+                Password = user.Password,
+                Role = user.Role,
+                Status = user.Status,
+                IsVerified = user.IsVerified,
+                DateCreated = user.DateCreated
+            }).ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetUserById(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+    }
+}
---
+++ FoodieFam-Back/DTOs/UserInsertDto.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodieFam_Back.DTOs
+{
+    public class UserInsertDto
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public bool Status { get; set; }
+        public bool IsVerified { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
